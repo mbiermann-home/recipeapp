dbd95b8ef876db2324c54ccd29dc56a4fb564701
mbierma <fertilesound@yahoo.com>
2020-01-18 16:57:52 -0600

    adding tests

---
+++ src/test/java/com/fertilesound/recipeapp/services/RecipeServiceImplTest.java
@@ -0,0 +1,46 @@
+package com.fertilesound.recipeapp.services;
+
+import com.fertilesound.recipeapp.domain.Recipe;
+import com.fertilesound.recipeapp.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+
+public class RecipeServiceImplTest {
+
+    RecipeService recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() {
+
+        Recipe recipe = new Recipe();
+        HashSet recipesData = new HashSet();
+        recipesData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(recipes.size(), 1);
+        Mockito.verify(recipeRepository, times(1)).findAll();
+    }
+}
