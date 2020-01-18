8478846d6e015c23625e6f9fe546c794ca3f5047
mbierma <fertilesound@yahoo.com>
2020-01-18 10:30:41 -0600

    added lombok and fixed all the issues it created.... :/

--- src/main/java/com/fertilesound/recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/com/fertilesound/recipeapp/services/RecipeServiceImpl.java
@@ -2,11 +2,13 @@ package com.fertilesound.recipeapp.services;
 
 import com.fertilesound.recipeapp.domain.Recipe;
 import com.fertilesound.recipeapp.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -19,7 +21,7 @@ public class RecipeServiceImpl implements RecipeService {
     @Override
     public Set<Recipe> getRecipes() {
         Set<Recipe> recipeSet = new HashSet<>();
-
+        log.debug("Getting recipes");
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
 
         return recipeSet;
